65c9e126528db140ebc4f742a2b6e2b587960ba7
HananFA <h.fathallah19@gmail.com>
2024-05-18 16:07:17 +0100

    I added Visiteur class and Admin class

---
+++ Models/User.php
@@ -0,0 +1,55 @@
+<?php
+require('Visiteur.php');
+class User extends Visiteur{
+    private $id ;
+    private $username;
+    private $email;
+    private $password;
+    private $photo_profile;
+
+    
+    public modif_username(){
+
+    }
+    public modif_photo_profile(){
+
+    }
+    public supp_photo(){
+
+    }
+    public ajout_plante_au_favoris(){
+
+    }
+    public supp_plante_au_favoris(){
+
+    }
+
+    public ajout_commentaire(){
+
+    }
+
+    public modif_commentaire(){
+
+    }
+
+    public supp_commentaire(){
+
+    }
+
+    public ajout_offre_de_vente(){
+
+    }
+
+    public modif_offre_de_vente(){
+
+    }
+    
+    public supp_offre_de_vente(){
+
+    }
+    public logout(){
+
+    }
+
+}
+?>
---
+++ Models/Visiteur.php
@@ -0,0 +1,23 @@
+<?php
+class Visiteur{
+    public login(){
+
+    }
+
+    public sign_up(){
+
+    }
+
+    public chercher_une_plante(){
+
+    }
+
+    public chercher_un_traitement(){
+
+    }
+    
+    public envoyer_message_ou_signal(){
+
+    }
+}
+?>
